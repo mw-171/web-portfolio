86d6e3e66d0d2726d5bd42f7f471c0d6f044b431
mw-171 <meganbxwu@gmail.com>
2024-01-30 20:56:48 -0500

    initializing new project

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="font-bold text-3xl">Hi!</div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
+        <div className="font-semibold "><code>under construction...</code></div>
       </header>
     </div>
   );
