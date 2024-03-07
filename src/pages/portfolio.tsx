5612ef502d9265d0b71ab68dc6f2e5f48427095b
mw-171 <meganbxwu@gmail.com>
2024-03-06 23:26:20 -0500

    set up basic gallrery

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -9,47 +9,35 @@ export default function Portfolio() {
     { src: "/gold.jpeg", alt: "hey" },
     { src: "/blue.jpeg", alt: "hey" },
     { src: "/gold.jpeg", alt: "hey" },
+    { src: "/gold.jpeg", alt: "hey" },
+    { src: "/blue.jpeg", alt: "hey" },
+    { src: "/gold.jpeg", alt: "hey" },
   ];
-  const column1 = images.slice(0, Math.ceil(images.length / 3));
+
+  const column1 = images.slice(0, Math.ceil(images.length / 4));
   const column2 = images.slice(
-    Math.ceil(images.length / 3),
-    Math.ceil((images.length * 2) / 3)
+    Math.ceil(images.length / 4),
+    Math.ceil((images.length * 2) / 4)
   );
-  const column3 = images.slice(Math.ceil((images.length * 2) / 3));
+  const column3 = images.slice(
+    Math.ceil((images.length * 2) / 4),
+    Math.ceil((images.length * 3) / 4)
+  );
+  const column4 = images.slice(Math.ceil((images.length * 3) / 4));
+
+  const imagesPerColumn = Math.ceil(images.length / 4);
 
   return (
     <div
       className="min-h-screen bg-cover bg-center overflow-auto"
       style={{ backgroundImage: "url('/test.jpg')" }}
     >
-      <div className="px-48 py-24">
-        <p className="font-semibold flex justify-start items-center text-5xl text-blue-300">
+      <div className="px-56 py-24">
+        <p className="pb-8 font-semibold flex justify-start items-center text-4xl text-black">
           Portfolio
         </p>
         {/* TODO: add gallery carousel here of projects, click into more specifics. need to develop components for individual display */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {images.map((image, index) => (
-            <div key={index} className="flex justify-center items-center">
-              <img
-                src={image.src}
-                alt={image.alt}
-                className="max-w-full h-auto"
-              />
-            </div>
-          ))}
-        </div>
-        <div className="flex flex-wrap -mx-4">
-          <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
-            {images.map((image) => (
-              <img
-                key={image.src}
-                src={image.src}
-                alt={image.alt}
-                className="h-auto object-cover rounded-lg max-w-full"
-              />
-            ))}
-          </div>
-        </div>
+
         <div className="flex flex-wrap">
           {/* Column 1 */}
           <div className="w-full md:w-1/3 lg:w-1/4 px-4">
@@ -89,6 +77,40 @@ export default function Portfolio() {
               </div>
             ))}
           </div>
+
+          {/* Column 4 */}
+          <div className="w-full md:w-1/2 lg:w-1/4 px-4">
+            {column4.map((image, index) => (
+              <div key={index} className="mb-4">
+                <img
+                  src={image.src}
+                  alt={image.alt}
+                  className="w-full h-auto"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex flex-wrap">
+          {[...Array(4)].map((_, columnIndex) => (
+            <div key={columnIndex} className="w-full md:w-1/2 lg:w-1/4 px-4">
+              {images
+                .slice(
+                  columnIndex * imagesPerColumn,
+                  (columnIndex + 1) * imagesPerColumn
+                )
+                .map((image, index) => (
+                  <div key={index} className="mb-4">
+                    <img
+                      src={image.src}
+                      alt={image.alt}
+                      className="w-full h-auto"
+                    />
+                  </div>
+                ))}
+            </div>
+          ))}
         </div>
       </div>
     </div>
