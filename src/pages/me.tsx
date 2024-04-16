2269749ad1cbe1dd5d67bf404707a63e8e17130e
mw-171 <meganbxwu@gmail.com>
2024-04-15 23:21:06 -0400

    doesnt work

--- src/pages/me.tsx
+++ src/pages/me.tsx
@@ -1,94 +1,36 @@
-import React from "react";
+import LocomotiveScroll from "locomotive-scroll";
+import { useEffect } from "react";
 
-const ScrollDirection: React.FC = () => {
+export default function Me() {
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: document.querySelector("[data-scroll-container]") as HTMLElement,
+      smooth: true,
+    });
+
+    return () => {
+      if (scroll) scroll.destroy();
+    };
+  }, []);
+
   return (
-    <div data-scroll-container>
-      <section className="c-section" data-scroll-section>
-        <div className="o-container" id="scroll-direction">
-          <div className="c-direction-block_wrapper">
-            <div
-              className="c-section_infos -padding"
-              data-scroll
-              data-scroll-sticky
-              data-scroll-target="#scroll-direction"
-            >
-              <div className="c-section_infos_inner" data-scroll>
-                <h3>
-                  02. <br />
-                  Scroll direction
-                </h3>
-                <div className="c-sections_infos_text u-text">
-                  <p>
-                    And if that wasn't enough, make 'em go backwards. Or
-                    upwards. Or downwards!
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="c-direction-block" id="direction">
-              <div className="c-direction-block_item -one">
-                <span
-                  className="c-direction-block_item_inner"
-                  data-scroll
-                  data-scroll-direction="horizontal"
-                  data-scroll-speed="6"
-                  data-scroll-target="#direction"
-                >
-                  I'm moving in this direction
-                </span>
-              </div>
-              <div className="c-direction-block_item -two">
-                <span
-                  className="c-direction-block_item_inner"
-                  data-scroll
-                  data-scroll-direction="horizontal"
-                  data-scroll-speed="-8"
-                  data-scroll-target="#direction"
-                >
-                  And in this direction
-                </span>
-              </div>
-              <div className="c-direction-block_item -three">
-                <span
-                  className="c-direction-block_item_inner"
-                  data-scroll
-                  data-scroll-direction="horizontal"
-                  data-scroll-speed="9"
-                  data-scroll-target="#direction"
-                  data-scroll-delay="0.05"
-                >
-                  Sooo customizable. Right?
-                </span>
-              </div>
-              <div className="c-direction-block_item -four">
-                <span
-                  className="c-direction-block_item_inner"
-                  data-scroll
-                  data-scroll-direction="horizontal"
-                  data-scroll-speed="15"
-                  data-scroll-target="#direction"
-                >
-                  I can also go in this direction
-                </span>
-              </div>
-              <div className="c-direction-block_item -five">
-                <span
-                  className="c-direction-block_item_inner"
-                  data-scroll
-                  data-scroll-direction="horizontal"
-                  data-scroll-speed="-20"
-                  data-scroll-target="#direction"
-                  data-scroll-delay="0.05"
-                >
-                  Ok, enough!!!!
-                </span>
-              </div>
-            </div>
-          </div>
+    <>
+      <div data-scroll-container>
+        <div data-scroll-section>
+          <h1 className="text-6xl" data-scroll>
+            Hey
+          </h1>
+          <p data-scroll>👋</p>
         </div>
-      </section>
-    </div>
+        <div data-scroll-section>
+          <h2 className="text-3xl" data-scroll data-scroll-speed="1">
+            What's up?
+          </h2>
+          <p data-scroll data-scroll-speed="2">
+            😬
+          </p>
+        </div>
+      </div>
+    </>
   );
-};
-
-export default ScrollDirection;
+}
