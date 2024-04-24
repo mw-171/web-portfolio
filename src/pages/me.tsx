a4cd840ee566a9ddf9b4229878e76b7486b33c68
mw-171 <meganbxwu@gmail.com>
2024-04-23 21:54:29 -0400

    add cool page

--- src/pages/me.tsx
+++ src/pages/me.tsx
@@ -12,14 +12,41 @@ export default function Me() {
   }, []);
 
   return (
-    <div className="py-48 flex items-center justify-center">
-      <div
-        className={`text-4xl font-semibold transition-opacity duration-150000 ${isVisible ? "opacity-100" : "opacity-0"}`}
-      >
-        hi!
+    <>
+      <div className="relative">
+        <div className="z-0 absolute top-[220px] left-[370px] animate-pulse">
+          <img
+            src="/10.png"
+            alt="blue star"
+            style={{ width: "20%", height: "auto", opacity: 1.5 }}
+          />
+        </div>
+        <div className="z-0 absolute top-[100px] left-[900px] animate-pulse">
+          <img
+            src="/9.png"
+            alt="purple stars"
+            style={{ width: "30%", height: "auto", opacity: 1.0 }}
+          />
+        </div>
+        <div className="z-0 absolute top-[320px] left-[850px] animate-pulse">
+          <img
+            src="/8.png"
+            alt="light blue stars"
+            style={{ width: "30%", height: "auto", opacity: 1.0 }}
+          />
+        </div>
       </div>
-      <div>this is going to be a cool easter egg!</div>
-    </div>
+      <div className="py-64 flex items-center justify-center flex-col ">
+        <div
+          className={`text-4xl text-gray-700 font-semibold z-20 transition-opacity duration-150000 ${isVisible ? "opacity-100" : "opacity-0"}`}
+        >
+          hi!
+        </div>
+        <div className="pt-4 text-gray-700">
+          this is going to be a cool easter egg!
+        </div>
+      </div>
+    </>
   );
 }
 
