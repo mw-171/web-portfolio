c7cf4fb69be0b9a9b8ccd073f7c6cfa688e845e9
mw-171 <meganbxwu@gmail.com>
2024-04-24 22:38:51 -0400

    rm scroll for now - will return!!!

--- src/pages/me.tsx
+++ src/pages/me.tsx
@@ -51,26 +51,3 @@ export default function Me() {
     </>
   );
 }
-
-// export default function Me() {
-//   return (
-//     <>
-//       <div data-scroll-container>
-//         <div data-scroll-section>
-//           <h1 className="text-6xl" data-scroll>
-//             Hey
-//           </h1>
-//           <p data-scroll>👋</p>
-//         </div>
-//         <div data-scroll-section>
-//           <h2 className="text-3xl" data-scroll data-scroll-speed="1">
-//             What's up!!
-//           </h2>
-//           <p data-scroll data-scroll-speed="2">
-//             😬
-//           </p>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
