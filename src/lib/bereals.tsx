84256df689e3ed4aacf61b87a0d55e0491374bb5
meganwu <meganbxwu@gmail.com>
2026-02-05 23:31:01 -0800

    remove some bereals

--- src/lib/bereals.tsx
+++ src/lib/bereals.tsx
@@ -26,16 +26,6 @@ export const images: ImageData[] = [
     alt: "frisbee game",
   },
   {
-    initialSrc: "/bereal/dinner1.JPG",
-    hoverSrc: "/bereal/dinner.JPG",
-    alt: "dinner",
-  },
-  {
-    initialSrc: "/bereal/postexam.JPG",
-    hoverSrc: "/bereal/postexam1.JPG",
-    alt: "post exam",
-  },
-  {
     initialSrc: "/bereal/hackthenorth.JPG",
     hoverSrc: "/bereal/hackthenorth1.JPG",
     alt: "organizing @ hack the north",
